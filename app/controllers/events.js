98be64af8c4fda45ebaf45751ab7cc1503c97076
Glenn Anderson <ganderson02@unomaha.edu>
2017-11-12 17:18:10 -0600

    added a calendar

---
+++ app/controllers/events.js
@@ -0,0 +1,9 @@
+import Controller from '@ember/controller';
+
+export default Controller.extend({
+  actions: {
+    clicked(event){
+      alert("TODO: make this look better, with the What/When/Where/Why information.");
+    }
+  }
+});
--- config/environment.js
+++ config/environment.js
@@ -7,6 +7,10 @@ module.exports = function(environment) {
     environment,
     rootURL: '/',
     locationType: 'auto',
+    emberFullCalendar: {
+      includeScheduler: true,
+      schedulerLicenseKey: 'GPL-My-Project-Is-Open-Source'
+    },
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
