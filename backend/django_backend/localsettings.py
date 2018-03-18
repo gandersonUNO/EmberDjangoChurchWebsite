3f4ae8120be192f117e2c0d8f22502fc4c7c64f8
gandersonUNO <ganderson02@unomaha.edu>
2018-03-18 18:38:33 -0500

    Update localsettings.py

--- backend/django_backend/localsettings.py
+++ backend/django_backend/localsettings.py
@@ -10,7 +10,7 @@ WSGI_APPLICATION = 'django_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'postgres',
         'USER': 'postgres',
         'HOST': 'db',
